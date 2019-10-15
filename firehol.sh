e43031555ed78f0cda0541bbc429508456e7a0e7
pedropetz <pedropigata@gmail.com>
2019-10-15 14:37:54 +0100

    final

--- firehol.sh
+++ firehol.sh
@@ -3,6 +3,6 @@
 DIR="/home/pihole/projectos/blocklists/"
 LIST="firehol"
 
-#wget -O "$DIR"lists/"$LIST" https://raw.githubusercontent.com/"$LIST"/blocklist-ipsets/master/"$LIST"_level3.netset > /dev/null 2> "$DIR"logs/"$LIST".err
+wget -O "$DIR"lists/"$LIST" https://raw.githubusercontent.com/"$LIST"/blocklist-ipsets/master/"$LIST"_level3.netset > /dev/null 2> "$DIR"logs/"$LIST".err
 
 date >> "$DIR"logs/"$LIST".txt
