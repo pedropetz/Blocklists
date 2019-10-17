409cab8bbeb53d5625ced0df8c5263d7ff661064
pedropetz <pedropigata@gmail.com>
2019-10-17 12:13:23 +0100

    update

--- blocklists_check.sh
+++ blocklists_check.sh
@@ -93,7 +93,60 @@ check_mask () {
     fi
     
 
-    }
+}
+
+function in_subnet {
+    # Determine whether IP address is in the specified subnet.
+    #
+    # Args:
+    #   sub: Subnet, in CIDR notation.
+    #   ip: IP address to check.
+    #
+    # Returns:
+    #   1|0
+    #
+    local ip ip_a mask netmask sub sub_ip rval start end
+
+    # Define bitmask.
+    local readonly BITMASK=0xFFFFFFFF
+
+    # Set DEBUG status if not already defined in the script.
+    [[ "${DEBUG}" == "" ]] && DEBUG=0
+
+    # Read arguments.
+    IFS=/ read sub mask <<< "${1}"
+    IFS=. read -a sub_ip <<< "${sub}"
+    IFS=. read -a ip_a <<< "${2}"
+
+    # Calculate netmask.
+    netmask=$(($BITMASK<<$((32-$mask)) & $BITMASK))
+
+    # Determine address range.
+    start=0
+    for o in "${sub_ip[@]}"
+    do
+        start=$(($start<<8 | $o))
+    done
+
+    start=$(($start & $netmask))
+    end=$(($start | ~$netmask & $BITMASK))
+
+    # Convert IP address to 32-bit number.
+    ip=0
+    for o in "${ip_a[@]}"
+    do
+        ip=$(($ip<<8 | $o))
+    done
+
+    # Determine if IP in range.
+    (( $ip >= $start )) && (( $ip <= $end )) && rval=1 || rval=0
+
+    (( $DEBUG )) &&
+        printf "ip=0x%08X; start=0x%08X; end=0x%08X; in_subnet=%u\n" $ip $start $end $rval 1>&2
+
+    echo "${rval}"
+}
+
 
 check_block_lists () {
     for list in "$DIRLSTS"/*;
@@ -107,7 +160,11 @@ check_block_lists () {
 	    echo "$CT/$NLINES"
 	    cat "$WHITELST" | while read -r whiteip
 	    do
-		check_mask "$whiteip" "$blockip" "$list"
+		if [ `in_subnet "$blockip" "$whiteip"` -eq 1 ]
+		then
+		    echo "$whiteip" "$blocklist"
+		    resolv_mask "$blockip" "$list"
+		fi
 	    done
 	done
 	check_white_lst $list
